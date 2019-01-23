767566014881accc16ab877f13edc0b0f05e6d12
jinghuichen <netfox001@gmail.com>
2019-01-23 16:10:49 -0500

    new version

--- test_attack.py
+++ test_attack.py
@@ -3,47 +3,51 @@ import numpy as np
 import random
 import time
  
+
+from EAD import EADEN
+
+from l2_attack import CarliniL2
+from li_attack import CarliniLi
+
+from IFGM import IFGM
 from FW import FW
+from PGD import PGD
 
 from classifier import classifier
 
 from PIL import Image
  
 
-def generate_data(data, samples, targeted=True, start=0, num_attacks = 1, inception=True):
-    """
-    Generate the input data to the attack algorithm.
-
-    data: the images to attack
-    samples: number of samples to use
-    targeted: if true, construct targeted attacks, otherwise untargeted attacks
-    start: offset into data to use
-    inception: if targeted and inception, randomly sample some targets  
-    """
+def generate_data(data, samples, targeted=True, start=0, num_attacks = 1, inception=False):
+     
     inputs = []
     targets = []
-    random.seed(1234)
+    # random.seed(1234)
     
     x = data.test_data
     y = data.test_labels
-    
-    # Filter samples that are correctly classified by the target classifier
+        
     cl = classifier(sess, model, loss_type = args.loss)
     eval_origin = cl.classify(x, y)
-    
+
     legal_ind = [ind for ind in range(len(eval_origin)) if eval_origin[ind]]
+    # print (len(legal_ind))
+    
+    # print (data.test_data.shape, data.test_labels.shape)
 
     data.test_data = data.test_data[legal_ind]
     data.test_labels = data.test_labels[legal_ind]
     
+    # print (data.test_data.shape, data.test_labels.shape)
+
     
     for i in range(samples):
         
         if targeted:
             if inception:
                 seq = random.sample(range(1,1001), num_attacks)
-                print ('Target Label: ', seq)
-                print ('Correct Label: ', np.argmax(data.test_labels[start+i]))
+                # print ('Target Label: ', seq)
+                # print ('Correct Label: ', np.argmax(data.test_labels[start+i]))
             else:
                 seq = [(np.argmax(data.test_labels[start+i]) + 1) % 10]
 
@@ -78,7 +82,7 @@ if __name__ == "__main__":
     parser.add_argument('--numatt', default=1, type=int, help='number of targeted attacks per image')
     parser.add_argument('--start', default=0, type=int, help='start image index')
     parser.add_argument('--lambd', default=5, type=float, help='lambda')
-
+    parser.add_argument('--out_steps', default=10, type=int, help='output steps')
 
 
     args = parser.parse_args()
@@ -93,33 +97,46 @@ if __name__ == "__main__":
         elif args.arch == 'inception':
             from setup_inception_v3 import ImageNet, inception_model
             data, model = ImageNet(), inception_model(sess)
-            print ('ImageNet Inception Model Loaded')            
+            print ('ImageNet Inception Model Loaded')    
+        elif args.arch == 'mnist':
+            from setup_mnist import MNIST, MNISTModel
+            data, model = MNIST(), MNISTModel("models/mnist",sess)
+            inception = 0
+            print ('MNIST Model Loaded')
         else:
             print ('Unknown Arch')
             import sys
             sys.exit()   
-                
+            
 
+        
+        print ('lr: ', args.lr)
+        print ('inception? ', inception)
+        
+ 
         if args.method == 'FW_L2':
             attack = FW(sess, model, batch_size=args.batch, ord=2, eps=args.eps, inception=inception, 
-                        nb_iter=args.maxiter, lr = args.lr, loss_type = args.loss, lambd = args.lambd)
+                        nb_iter=args.maxiter, lr = args.lr, loss_type = args.loss, lambd = args.lambd,
+                        output_steps = args.out_steps)
         elif args.method == 'FW_Linf':
             attack = FW(sess, model, batch_size=args.batch, ord=np.inf, eps=args.eps, inception=inception, 
-                        nb_iter=args.maxiter, lr = args.lr, loss_type = args.loss, lambd = args.lambd)
+                        nb_iter=args.maxiter, lr = args.lr, loss_type = args.loss, lambd = args.lambd,
+                        output_steps = args.out_steps)
         else:
             print ('Unknown Attack Methods')
             import sys
             sys.exit()
                
-        print('Generating data')
+        
+        print('Generate data')
         inputs, targets = generate_data(data, samples=args.sample, targeted=True, start=args.start, num_attacks = args.numatt,
                                         inception=inception)
+        print('Inputs Shape: ', inputs.shape)
         
-                
         timestart = time.time()
         adv, time_images, succ_rate, stop_iter = attack.attack(inputs, targets)
         timeend = time.time()
         
         print("Took",timeend-timestart,"seconds to run",len(inputs),"samples.")
 
-   
+ 
--- test_attack_black.py
+++ test_attack_black.py
@@ -2,7 +2,10 @@ import tensorflow as tf
 import numpy as np
 import random
 import time
- 
+
+
+from l2_attack_black import BlackBoxL2
+from IFGM_black import IFGM_black
 from FW_black import FW_black
 from classifier import classifier
 
@@ -11,39 +14,33 @@ from PIL import Image
  
 
 def generate_data(data, samples, targeted=True, start=0, num_attacks = 1, inception=False):
-    """
-    Generate the input data to the attack algorithm.
 
-    data: the images to attack
-    samples: number of samples to use
-    targeted: if true, construct targeted attacks, otherwise untargeted attacks
-    start: offset into data to use
-    inception: if targeted and inception, randomly sample some targets  
-    """
     inputs = []
     targets = []
-    random.seed(1234)
+    # random.seed(1234)
     
     x = data.test_data
     y = data.test_labels
-    
-    # Filter samples that are correctly classified by the target classifier    
+        
     cl = classifier(sess, model, loss_type = args.loss)
     eval_origin = cl.classify(x, y)
-
     legal_ind = [ind for ind in range(len(eval_origin)) if eval_origin[ind]]
+    # print (len(legal_ind))
+    
+    # print (data.test_data.shape, data.test_labels.shape)
 
     data.test_data = data.test_data[legal_ind]
     data.test_labels = data.test_labels[legal_ind]
-
+    
+    # print (data.test_data.shape, data.test_labels.shape)
     
     for i in range(samples):
         
         if targeted:
             if inception:
                 seq = random.sample(range(1,1001), num_attacks)
-                print ('Target Label: ', seq)
-                print ('Correct Label: ', np.argmax(data.test_labels[start+i]))
+                # print ('Target Label: ', seq)
+                # print ('Correct Label: ', np.argmax(data.test_labels[start+i]))
             else:
                 seq = [(np.argmax(data.test_labels[start+i]) + 1) % 10]
             for j in seq:
@@ -66,7 +63,7 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--lr', default=0.01, type=float, help='FW learning rate')
-    parser.add_argument('--method', help='attack method')
+    parser.add_argument('--method', '-m', help='attack method')
     parser.add_argument('--arch', '-a', default='inception', help='attack architecture')    
     parser.add_argument('--loss', default='cross_entropy', help='loss function type: cross_entropy / cw')
     parser.add_argument('--grad_est_batch', default=25, type=int, help='gradient estimation batch size')
@@ -80,6 +77,7 @@ if __name__ == "__main__":
     parser.add_argument('--init_const', default=10, type=float, help='initial_const for ZOO')
     parser.add_argument('--bin_step', default=1, type=int, help='binary search steps for ZOO')
     parser.add_argument('--lambd', default=30, type=float, help='lambda')
+    parser.add_argument('--out_steps', default=10, type=int, help='output steps')
 
 
     args = parser.parse_args()
@@ -96,31 +94,45 @@ if __name__ == "__main__":
             from setup_inception_v3 import ImageNet, inception_model
             data, model = ImageNet(), inception_model(sess)
             print ('ImageNet Inception Model Loaded')            
+        elif args.arch == 'cifar':
+            from setup_cifar import CIFAR, CIFARModel
+            data, model = CIFAR(), CIFARModel("models/cifar",sess)
+            inception = 0
+            print ('CIFAR10 Model Loaded')
+        elif args.arch == 'mnist':
+            from setup_mnist import MNIST, MNISTModel
+            data, model = MNIST(), MNISTModel("models/mnist",sess)
+            inception = 0
+            print ('MNIST Model Loaded')
         else:
             print ('Unknown Arch')
             import sys
-            sys.exit() 
- 
- 
+            sys.exit()   
+            
+            
+        # print ('lr: ', args.lr)
+        # print ('inception? ', inception)
+
         if args.method == 'FW_L2':
             attack = FW_black(sess, model, grad_est_batch_size=args.grad_est_batch, ord=2, eps=args.eps, 
                               inception=inception, nb_iter=args.maxiter, lr = args.lr, loss_type = args.loss, 
-                              sensing_type = args.sensing, delta = args.delta, lambd = args.lambd)
+                              sensing_type = args.sensing, delta = args.delta, lambd = args.lambd, 
+                              output_steps = args.out_steps)
         elif args.method == 'FW_Linf':
             attack = FW_black(sess, model, grad_est_batch_size=args.grad_est_batch, ord=np.inf, eps=args.eps, 
                               inception=inception, nb_iter=args.maxiter, lr = args.lr, loss_type = args.loss, 
-                              sensing_type = args.sensing, delta = args.delta, lambd = args.lambd)
+                              sensing_type = args.sensing, delta = args.delta, lambd = args.lambd, 
+                              output_steps = args.out_steps)
         else:
             print ('Unknown Attack Methods')
             import sys
             sys.exit()
                
 
-        print('Generating data')
+        print('Generate data')
         inputs, targets = generate_data(data, samples=args.sample, targeted=True, start=args.start, num_attacks = args.numatt,
                                         inception=inception)
-
-        
+        print('Inputs Shape: ', inputs.shape)
         
         timestart = time.time()
         adv, query_images, time_images, succ_rate = attack.attack(inputs, targets)
@@ -128,4 +140,8 @@ if __name__ == "__main__":
         
         print("Took",timeend-timestart,"seconds to run",len(inputs),"samples.")
 
-  
+ 
+        print ("adv shape: ", adv.shape)
+        print("Total distortion:", np.sum((adv[0]-inputs[0])**2)**.5)
+        
+ 
